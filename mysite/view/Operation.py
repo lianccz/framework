de147f57f66cda5df86c5406d2335ffa6927c0ec
root <root@VM_0_5_centos.(none)>
2017-11-27 14:10:22 +0800

    4

--- mysite/view/Operation.py
+++ mysite/view/Operation.py
@@ -17,7 +17,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-Android_name = "/data/xml_file/SerAreaConfig.xml"
+Android_name = "/data/xml_file/SerAreaConfig_Android.xml"
 IOS_name = "/data/xml_file/SerAreaConfig_IOS.xml"
 test_name = "/data/xml_file/SerAreaConfig_test.xml"
 # 获取xml所有值
