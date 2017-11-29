d6176d73e730c338f1da1df79a4b977410f13774
root <root@VM_0_5_centos.(none)>
2017-11-29 15:05:40 +0800

    8

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -12,9 +12,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'backup',                            # 你的数据库user
-        'PASSWORD': 'passwd',                        # 你的数据库password
-        'HOST': '172.29.0.5',                   		   # 数据库HOST
+        'USER': '',                            # 你的数据库user
+        'PASSWORD': '',                        # 你的数据库password
+        'HOST': '',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
